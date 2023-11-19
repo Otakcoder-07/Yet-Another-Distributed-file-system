8d82404bfe4e278b79c6da627a083ed258bff30c
Shamith <chandrashamith@gmail.com>
2023-11-19 18:33:29 +0530

    Upload implemented by Shreyas bandi , download implemented by Shamith , metadata handling and storage by suraj , other functionalities by shreyas m,only thing remaining is fixing bugs due to recent changes

---
+++ client.py
@@ -0,0 +1,144 @@
+import rpyc
+import os
+import argparse
+
+class Client:
+    def __init__(self, file_path):
+        self.conn = rpyc.connect("127.0.0.1", 12345)
+        self.file_path = file_path
+
+    def send_message(self, message):
+        self.conn.root.receive_message(message)
+
+    def send_file_name_to_nn(self):
+        self.conn.root.update_filename(self.file_path)
+
+    def upload_file(self, path, ip_addr_array,block_size):
+        lis=[]
+        print(block_size)
+        blocks = self.split_file_fixed_size(block_size)
+        print(f"total Blocks {len(blocks)}")
+        ip_addr = ip_addr_array['block_locations'][0]
+        
+        # k = self.upload_block(ip_addr['ip_addr'], ip_addr['port'], path, message, ip_addr_array)
+
+        for i, block in enumerate(blocks):
+            k = self.upload_block(ip_addr['ip_addr'], ip_addr['port'], path, block, ip_addr_array)
+            lis.append(k)
+        print('\n\n\n\n\n\n')
+        self.ping_nn_after_upload()
+        return lis
+        
+
+    def upload_block(self, ip_addr, portno, path, message, ip_addr_array):
+
+        conn = rpyc.connect(ip_addr, portno)
+        num = conn.root.receive_message(path, message, ip_addr_array,self.file_path)
+        return list(num['data'])
+
+    def split_file_fixed_size(self, block_size):
+        blocks = []
+
+        with open(self.file_path, 'rb') as file:
+            while True:
+                data = file.read(block_size)
+                if not data:
+                    break
+                blocks.append(data)
+        return blocks
+
+    def get_metadata(self):
+        return self.conn.root.write_file()
+    
+    def ping_nn_after_upload(self):
+        self.conn.root.upload_complete()
+        print('\n\ndone\n\n')
+
+    def download_file(self, file_name):
+        metadata = self.get_metadata()
+        print("\n\nmetadata:\n\n",metadata)
+        block_detail = self.get_blockname_data(file_name)
+        print("block_detail:\n",block_detail)
+        if not metadata or "block_locations" not in metadata:
+            print(f"File '{file_name}' not found or metadata is missing.")
+            return
+
+        # Step 2: Retrieve block locations
+        block_locations = metadata["block_locations"]
+
+        # Step 3: Retrieve data blocks from DataNodes
+        file_data = b""
+        for block_location in block_locations:
+            working_datanode = False
+            ip_addr = block_location["ip_addr"]
+            port_no = block_location["port"]
+            # block_name = block_location["block_name"]
+            # block_name = 
+            # Connect to the DataNode
+            data_node_conn = rpyc.connect(ip_addr, port_no)
+            block_index = 1
+            # Request the data block from the DataNode
+            while True:
+                # block_name = 
+                if str(block_index) in block_detail:
+                    block_name = block_detail[str(block_index)][0]
+                    print("\n\n\n\n\n")
+                    print(block_name)
+                    data_block = data_node_conn.root.retrieve_data_block(block_name)
+                    print('block : ',data_block)
+                    file_data += data_block
+                    block_index+=1
+                    # block_name = str(block_index)
+
+                else:
+                    print('not there')
+                    break
+            data_node_conn.close()
+            # print('filedata : ',file_data)
+            break
+
+            # Close the connection to the DataNode
+
+        # Step 4: Reassemble the file
+        file_path = os.path.join(os.getcwd(), "downloads", file_name)
+        with open(file_path, "wb") as output_file:
+            output_file.write(file_data)
+        # Step 4: Reassemble the file
+
+        print(f"File '{file_name}' downloaded successfully.")
+
+    def get_data_nodes(self):
+        return self.conn.root.write_file()
+
+    def get_blockname_data(self,filename):
+        dock = self.conn.root.get_blockname_data(filename)
+        return dock
+    def close_connection(self):
+        self.conn.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Client for uploading and downloading files from a distributed file system.")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    put_parser = subparsers.add_parser("put", help="Upload a file")
+    put_parser.add_argument("file_name", help="Name of the file to upload")
+
+    get_parser = subparsers.add_parser("get", help="Download a file")
+    get_parser.add_argument("file_name", help="Name of the file to download")
+
+    args = parser.parse_args()
+
+    if args.command == "put":
+        client = Client(args.file_name)
+        block_size= 500  # Adjust to your preferred fixed block size
+        lis = client.get_data_nodes()
+        print(len(lis['block_locations']))
+        client.send_file_name_to_nn()
+        
+        client.upload_file("one", lis,block_size)
+
+        client.close_connection()
+    elif args.command == "get":
+        client = Client("")
+        client.download_file(args.file_name)
+        client.close_connection()
---
+++ namenode.py
@@ -0,0 +1,124 @@
+import rpyc
+from pymongo import MongoClient
+import threading
+from rpyc.utils.server import ThreadedServer
+import bson
+import traceback
+import sys
+from datetime import datetime
+import time
+
+
+datablocks = []
+
+
+class NameNode(rpyc.Service):
+    datanode_no = 0
+    datanode_id = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    datanode_no1 = 0
+    global datablocks
+    def on_connect(self, conn):
+        self.mongo = MongoClient('mongodb://localhost:27017/')
+        db = self.mongo['bdprojectdb']
+        self.coll = db['metadata'] 
+        self.dn_coll = db['datanode_detail']
+        print('Metadata connected')
+        print("NameNode connected")
+
+    def on_disconnect(self, conn):
+        print("NameNode disconnected")
+
+    def exposed_update_filename(self,file_name):
+        self.filename = file_name
+        dict = {}
+        dict['file_name'] = file_name
+        d = self.coll.find_one(dict)
+        if(d == None):
+            self.coll.insert_one(dict)
+        else:
+            print(f'file {self.filename} already exists')
+            sys.exit(0)
+
+    def exposed_write_file(self):
+        metadata = [
+            {"ip_addr": "192.168.56.1", "port": 12346, "block_name": "0.txt"},
+            {"ip_addr": "192.168.56.1", "port": 12347, "block_name": "50.txt"},
+            {"ip_addr": "192.168.56.1", "port": 12348, "block_name": "100.txt"}
+        ]
+
+        #loading metadata
+        k = self.dn_coll.find_one({'id':self.datanode_id})
+        return {"block_locations": k['block_locations']}
+
+    def exposed_receive_message(self, message):
+        message['block_name'] = str(self.datanode_no1)+'.txt'
+        self.datanode_no1 += 1
+        self.datanode = []
+        self.datanode.append(message)
+        # print(f"Received message from client: {self.datanode}")
+
+    def exposed_update_block_names(self,block_data):
+        print('block_no : ',self.datanode_no)
+        self.datanode_no += 1
+        dock = self.coll.find_one({'file_name':self.filename})
+        dock[str(self.datanode_no)] = list(block_data['data'])
+        self.coll.update_one({'file_name': self.filename}, {'$set': {str(self.datanode_no): list(block_data['data'])}})
+
+    def exposed_upload_complete(self):
+        self.datanode_no = 0
+        dock = self.coll.find_one({'file_name':self.filename})
+        print(f'filename : {self.filename},block_data : {dock}')
+
+    def exposed_get_blockname_data(self,filename):
+        dock = self.coll.find_one({'file_name':filename})
+        return dock
+
+    def exposed_mark_datanode(self,ip_dn,port_dn):
+        print(f'datanode :\nip : {ip_dn},port : {port_dn}')
+        detail = {'ip_addr':ip_dn,'port':port_dn}
+        dock = self.dn_coll.find_one({'id':self.datanode_id})
+        if(dock):
+            temp = dock['block_locations']
+            temp.append(detail)
+            self.dn_coll.update_one({'id':self.datanode_id},{'$set':{'block_locations':temp}})
+        else:
+            dict = {'id':self.datanode_id,'block_locations':[detail]}
+            self.dn_coll.insert_one(dict)
+            time.sleep(3)
+            self.is_datanode_alive()
+
+    def is_datanode_alive(self):
+        dn_detail = self.dn_coll.find_one({'id':self.datanode_id})
+        print(f'dn in alive{dn_detail}')
+        index = 0
+        for dn_det in dn_detail['block_locations']:
+            ip_addr = dn_det['ip_addr']
+            port = dn_det['port']
+            try:
+                rpyc.connect(ip_addr,port)
+            except ConnectionRefusedError as e:
+                print(f'datanode with ip : {ip_addr} and port : {port} is lost and reason {e}')
+                dn_detail['block_locations'].pop(index)
+                self.dn_coll.update_one({'id':self.datanode_id},{'$set':{'block_locations':dn_detail['block_locations']}})
+            finally:
+                index += 1
+        time.sleep(500)
+        self.is_datanode_alive()
+
+
+
+
+if __name__ == "__main__":
+
+    # # namenode = NameError()
+    # # thre = threading.Thread(target = namenode)
+    # t = ThreadedServer(NameNode(), port=12345)
+    # t.start()
+    try:
+        t = ThreadedServer(NameNode(), port=12345)
+        t.start()
+    except Exception as e:
+        print(f"Error starting NameNode server: {e}")
+        traceback.print_exc()
+
